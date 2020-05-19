2171c21ddf58bdcfa6c5bb5703224fbc5833d9be
Patrick <mailtopatrick@gmx.net>
2020-05-19 20:32:49 +0200

    Added xcode projec5

---
+++ working/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++ training
+//
+//  Created by Patrick Fischer on 19.05.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
